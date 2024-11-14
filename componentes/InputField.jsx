2bf87d8ffb27f10f4b9eae49b9c3a4503c146780
Abril <sepulveda.abril.epet20@gmail.com>
2024-11-14 10:04:54 -0300

    Agregué estilos para los tetxos de error en InputField

--- componentes/InputField.jsx
+++ componentes/InputField.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TextInput, StyleSheet, Text, View } from 'react-native';
 
+
 export default function InputField({ placeholder, value, onChangeText, keyboardType = 'default', secureTextEntry = false, error }) {
     return (
         <View style={styles.container}>
@@ -16,3 +17,28 @@ export default function InputField({ placeholder, value, onChangeText, keyboardT
         </View>
     );
 }
+
+
+//estilos
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+    },
+    input: {
+        width: '100%',
+        padding: 15,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 8,
+        marginBottom: 15,
+    },
+    inputError: {
+        borderColor: 'red',
+    },
+    errorText: {
+        color: 'red',
+        fontSize: 12,
+        marginTop: -10,
+        marginBottom: 10,
+    },
+});
