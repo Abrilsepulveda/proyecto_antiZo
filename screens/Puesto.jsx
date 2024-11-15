5c8e6167429585619ac790f55bb849c2e46e9f2d
May-Zaracho <zarachomaylen555@gmail.com>
2024-11-15 06:00:31 -0300

    Agrege los Imports de Pusto

---
+++ screens/Puesto.jsx
@@ -0,0 +1,5 @@
+       
+       import React from 'react';
+       import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+       import { useNavigation } from '@react-navigation/native';
+       
